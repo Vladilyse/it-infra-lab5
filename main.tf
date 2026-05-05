b2e2f82777d3adfc48aecec14bd7612683a48ad9
Vladilyse <92036521+Vladilyse@users.noreply.github.com>
2026-05-05 23:18:49 +0530

    commit

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "aws_instance" "web_server" {
   vpc_security_group_ids = [aws_security_group.lab6_sg.id]
 
   # Переконайся, що створив цей ключ у консолі регіону us-east-1
-  key_name               = "lab5-key" 
+  key_name               = "terraform-lab-key" 
 
   user_data = <<-EOF
               #!/bin/bash
